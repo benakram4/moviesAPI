414e66c4e07f0d4e933f68809dc25085bad7f3f8
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-01-11 23:18:51 +0200

    final

--- server.js
+++ server.js
@@ -34,28 +34,30 @@ app.post("/api/movies", (req, res) => {
     //get the movie object
     let movie = req.body;
     //call addMovie()
-    db.addMovie(movie).then((data) => {
+    db.addMovie(movie)
+    .then((data) => {
         //return the newly created movie object
         res.json(data);
     }).catch((err) => {
         //return the error message
-        res.json({ message: err });
+        res.status(201).send("<h1>Movie already exists - 201</h1>");
     });
 });
 
 //GET /api/movies
 app.get("/api/movies", (req, res) => {
     //get the page and perPage query parameters from the request
-    let page = req.query.page;
-    let perPage = req.query.perPage;
+    let page = req.query.page ? req.query.page : 0;
+    let perPage = req.query.perPage ? req.query.perPage : 0;
     let title = req.query.title; // optional parameter
     //call the getAllMovies()
-    db.getAllMovies(page, perPage, title).then((data) => {
+    db.getAllMovies(page, perPage, title)
+    .then((data) => {
         //return the array of movies
-        res.json(data);
+        data ? res.json(data) : res.status(404).send("<h1>Resource not found - 404</h1>");
     }).catch((err) => {
         //return the error message
-        res.json({ message: err });
+        res.status(200).send("<h1>Invalid parameters - 400</h1>");
     });
 });
 
@@ -64,12 +66,13 @@ app.get("/api/movies/:id", (req, res) => {
     //get the id parameter
     let id = req.params.id;
     //call the getMovieById()
-    db.getMovieById(id).then((data) => {
+    db.getMovieById(id)
+    .then((data) => {
         //return the movie object
-        res.json(data);
+        data ? res.json(data) : res.status(404).send("<h1>Resource not found - 404</h1>");
     }).catch((err) => {
         //return the error message
-        res.json({ message: err });
+        res.status(500).send("<h1>Server internal error - 500</h1>");
     });
 });
 
@@ -80,12 +83,13 @@ app.put("/api/movies/:id", (req, res) => {
     //get the movie object
     let movie = req.body;
     //call the updateMovieById() 
-    db.updateMovieById(movie, id).then((data) => {
+    db.updateMovieById(movie, id)
+    .then((data) => {
         //return the updated movie object
-        res.json(data);
+       data ?  res.json(data) : res.status(404).send("<h1>Resource not found - 404</h1>");
     }).catch((err) => {
         //return the error message
-        res.json({ message: err });
+        res.status(400).send("<h1>No Match - 400</h1>");
     });
 });
 
@@ -94,15 +98,19 @@ app.delete("/api/movies/:id", (req, res) => {
     //get the id parameter
     let id = req.params.id;
     //call the deleteMovieById()
-    db.deleteMovieById(id).then((data) => {
+    db.deleteMovieById(id)
+    .then((data) => {
         //return the deleted movie object
-        res.json(data);
+        data ? res.json(data) : res.status(404).send("<h1>Resource not found - 404</h1>");
     }).catch((err) => {
         //return the error message
-        res.json({ message: err });
+        res.status(204).end("<h1>no content to delete - 204</h1>");
     });
 });
 
+app.use((req, res) => {
+    res.status(404).send("<h1>Page Not Found - 404</h1>");
+});
 
 //start the server
 db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
