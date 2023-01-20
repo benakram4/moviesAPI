7e3155d018bf4ca8d7c7e82b52cb9c2580df1f5e
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-01-19 21:25:17 -0500

    signature added

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/********************************************************************************* 
+*  BTI425 – Assignment 1 
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.   
+*  No part of this assignment has been copied manually or electronically from any other source 
+*  (including web sites) or distributed to other students. 
+*  
+*  Name: Ben Akram Student ID: 158523217 Date: 19 January 2023
+*  Cyclic Link: https://zany-teal-narwhal-cap.cyclic.app
+* 
+********************************************************************************/ 
 
 var express = require('express');
 var app = express();
