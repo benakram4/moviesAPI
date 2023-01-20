583f610f71fc2b66935b02a3717b76bf0fe307bb
benakram4 <105305419+benakram4@users.noreply.github.com>
2023-01-19 21:18:59 -0500

    final

--- server.js
+++ server.js
@@ -30,30 +30,27 @@ db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
     console.log(err);
 });
 
-//create a route for the app to get index.html
+//Home route
 app.get('/', function (req, res) {
-    //res.sendFile(__dirname + '/index.html');
-
-    //return the following object (JSON): {message: "API Listening"}. 
     res.json({ message: "API Listening" });
 });
 
-//POST /api/movies
+//POST - C - Add a new movie
 app.post("/api/movies", (req, res) => {
     //get the movie object
     let movie = req.body;
     //call addMovie()
     db.addMovie(movie)
     .then((data) => {
-        //return the newly created movie object
+        //return new movie object
         res.status(201).json(data);
     }).catch((err) => {
         //return the error message
-        res.status(500).json({ message: `error: ${err} - 500` });
+        res.status(500).json({ message: `Error - 500: ${err} ` });
     });
 });
 
-//GET /api/movies - get all movies
+//GET - R - Get all movies
 app.get("/api/movies", (req, res) => {
     //get the page and perPage query parameters from the request
     let page = req.query.page ? req.query.page : 0;
@@ -66,26 +63,26 @@ app.get("/api/movies", (req, res) => {
         data ? res.status(200).json(data) : res.status(404).json({ message: "Resource not found - 404" });
     }).catch((err) => {
         //return the error message
-        res.status(500).json({ message: `No movies found - error: ${err} - 500` });
+        res.status(500).json({ message: `No movies found - Error - 500: ${err} ` });
     });
 });
 
-//GET /api/movies/:id - get a movie by id
+//GET - R - Get one movie by id
 app.get("/api/movies/:id", (req, res) => {
     //get the id parameter
     let id = req.params.id;
-    //call the getMovieById()
+    //call getMovieById()
     db.getMovieById(id)
     .then((data) => {
         //return the movie object
         data ? res.status(200).json(data) : res.status(404).json({ message: "Resource not found - 404" });
     }).catch((err) => {
         //return the error message
-        res.status(500).json({message: `No movies found - error: ${err} - 500`});
+        res.status(500).json({message: `Resource not found - Error - 500: ${err}`});
     });
 });
 
-//PUT /api/movies/:id
+//PUT - U - Update a movie by id
 app.put("/api/movies/:id", (req, res) => {
     //get the id parameter
     let id = req.params.id;
@@ -98,27 +95,27 @@ app.put("/api/movies/:id", (req, res) => {
         data ? res.status(200).json(data) : res.status(404).json({ message: "Resource not found - 404" });
     }).catch((err) => {
         //return the error message
-        res.status(400).json({message: "No Match - 400"});
+        res.status(400).json({message: `No Match - Error - 400: ${err}`});
     });
 });
 
-//DELETE /api/movies/:id
+//DELETE - D - Delete a movie by id
 app.delete("/api/movies/:id", (req, res) => {
     //get the id parameter
     let id = req.params.id;
-    //call the deleteMovieById()
+    //call deleteMovieById()
     db.deleteMovieById(id)
     .then((data) => {
         //return the deleted movie object
         data ? res.status(204).end() : res.status(404).json({ message: "Resource not found - 404" });
     }).catch((err) => {
         //return the error message
-        res.status(500).json({ message: `No movies found - error: ${err} - 500`});
+        res.status(500).json({ message: `Resource not found - Error - 500: ${err}`});
     });
 });
 
 app.use((req, res) => {
-    res.status(404).send("<h1>Page Not Found - 404</h1>");
+    res.status(404).send(`<h1>Page Not Found - 404</h1>`);
 });
 
 
